c50cc1ff56a427abfb93d66a07b5ad63d3de9c67
nitin-codr <nitinporwal99@gmail.com>
2021-03-10 13:45:19 +0530

    register the user via email in firebase

--- app/src/main/java/com/example/projemanage/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/SignUpActivity.kt
@@ -6,6 +6,8 @@ import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Toast
 import com.example.projemanage.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
@@ -43,7 +45,24 @@ class SignUpActivity : BaseActivity() {
         val password: String = et_password.text.toString().trim { it <= ' ' }
 
         if (validateForm(name, email, password)) {
-            Toast.makeText(this, "Now we can register a new user", Toast.LENGTH_SHORT).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email!!
+                        Toast.makeText(
+                            this,
+                            "$name you have successfully registered the email address $registeredEmail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 
