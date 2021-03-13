c37d69e681093c3044cfd4982e872d3ecf0d8aa2
nitin-codr <nitinporwal99@gmail.com>
2021-03-14 02:32:36 +0530

    implement image chooser for the profile image

--- app/src/main/java/com/example/projemanage/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/ProfileActivity.kt
@@ -1,14 +1,32 @@
 package com.example.projemanage.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.projemanage.R
 import com.example.projemanage.firebase.FirestoreClass
 import com.example.projemanage.models.User
 import kotlinx.android.synthetic.main.activity_profile.*
+import java.io.IOException
+import java.util.jar.Manifest
 
 class ProfileActivity : BaseActivity() {
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -16,6 +34,59 @@ class ProfileActivity : BaseActivity() {
         setupActionBar()
 
         FirestoreClass().loadUserData(this)
+
+        my_profile_user_image.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "Oops, you just denied the permission for storage. You can allow it from settings.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun showImageChooser() {
+        var galleryIntern = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntern, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data != null) {
+            mSelectedImageFileUri = data.data
+            try {
+                Glide.with(this).load(mSelectedImageFileUri).centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(my_profile_user_image)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun setupActionBar() {
