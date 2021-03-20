bfccd82fe9e498949ef0400d59cd61ba72ccbb13
nitin-codr <nitinporwal99@gmail.com>
2021-03-20 18:46:58 +0530

    prepare the tasklist adapter and link it to tasklist activity

--- app/src/main/java/com/example/projemanage/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/TaskListActivity.kt
@@ -1,9 +1,12 @@
 package com.example.projemanage.activities
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projemanage.R
+import com.example.projemanage.adapters.TaskListItemsAdapter
 import com.example.projemanage.firebase.FirestoreClass
 import com.example.projemanage.models.Board
+import com.example.projemanage.models.Task
 import com.example.projemanage.utils.Constants
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_task_list.*
@@ -40,5 +43,15 @@ class TaskListActivity : BaseActivity() {
     fun boardDetails(board: Board) {
         hideProgressDialog()
         setupActionBar(board.name)
+
+        val addTaskList = Task(resources.getString(R.string.add_list))
+        board.taskList.add(addTaskList)
+
+        rv_task_list.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rv_task_list.setHasFixedSize(true)
+
+        val adapter = TaskListItemsAdapter(this, board.taskList)
+        rv_task_list.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/projemanage/adapters/TaskListItemsAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.projemanage.adapters
+
+import android.content.Context
+import android.content.res.Resources
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projemanage.R
+import com.example.projemanage.models.Task
+import kotlinx.android.synthetic.main.item_task.view.*
+
+open class TaskListItemsAdapter(private val context: Context, private var list: ArrayList<Task>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_task, parent, false)
+        val layoutParams = LinearLayout.LayoutParams(
+            (parent.width * .7).toInt(), LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins((15.toDp().toPx()), 0, (40.toDp()).toPx(), 0)
+        view.layoutParams = layoutParams
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+        if (holder is MyViewHolder) {
+            if (position == list.size - 1) {
+                holder.itemView.tv_add_task_list.visibility = View.VISIBLE
+                holder.itemView.ll_task_item.visibility = View.GONE
+            } else {
+                holder.itemView.tv_add_task_list.visibility = View.GONE
+                holder.itemView.ll_task_item.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    private class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    private fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
+    private fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+}
