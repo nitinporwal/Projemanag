08104d11d51665917eb8e3d9d21a292b18d93b19
nitin-codr <nitinporwal99@gmail.com>
2021-03-25 03:12:23 +0530

    delete card and update card name

--- app/src/main/java/com/example/projemanage/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/CardDetailsActivity.kt
@@ -1,9 +1,16 @@
 package com.example.projemanage.activities
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.projemanage.R
+import com.example.projemanage.firebase.FirestoreClass
 import com.example.projemanage.models.Board
+import com.example.projemanage.models.Card
+import com.example.projemanage.models.Task
 import com.example.projemanage.utils.Constants
 import kotlinx.android.synthetic.main.activity_card_details.*
 
@@ -22,8 +29,22 @@ class CardDetailsActivity : BaseActivity() {
 
         et_name_card_details.setText(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
         et_name_card_details.setSelection(et_name_card_details.text.toString().length)
+
+        btn_update_card_details.setOnClickListener {
+            if (et_name_card_details.text.toString().isNotEmpty()) {
+                updateCardDetails()
+            } else {
+                Toast.makeText(this, "Enter a card name", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
+    fun addUpdateTaskListSuccess() {
+        hideProgressDialog()
+
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
 
     private fun setupActionBar() {
         setSupportActionBar(toolbar_card_details_activity)
@@ -44,6 +65,18 @@ class CardDetailsActivity : BaseActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_delete_card -> {
+                alertDialogForDeleteCard(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
+                return true
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun getIntentData() {
         if (intent.hasExtra(Constants.BOARD_DETAIL)) {
             mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
@@ -54,6 +87,49 @@ class CardDetailsActivity : BaseActivity() {
         if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
             mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
         }
+    }
 
+    private fun updateCardDetails() {
+        val card = Card(
+            et_name_card_details.text.toString(),
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].createdBy,
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo
+        )
+        mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition] = card
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this, mBoardDetails)
+    }
+
+    private fun deleteCard() {
+        val cardList: ArrayList<Card> = mBoardDetails.taskList[mTaskListPosition].cards
+        cardList.removeAt(mCardPosition)
+        val taskList: ArrayList<Task> = mBoardDetails.taskList
+        taskList.removeAt(taskList.size-1)
+        taskList[mTaskListPosition].cards = cardList
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this, mBoardDetails)
+    }
+
+    private fun alertDialogForDeleteCard(cardName: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(resources.getString(R.string.alert))
+        builder.setMessage(
+            resources.getString(R.string.confirmation_message_to_delete_card, cardName)
+        )
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton(resources.getString(R.string.yes)) { dialogInterface, which ->
+            dialogInterface.dismiss()
+            deleteCard()
+        }
+
+        builder.setNegativeButton(resources.getString(R.string.no)) { dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
--- app/src/main/java/com/example/projemanage/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/projemanage/firebase/FirestoreClass.kt
@@ -80,16 +80,26 @@ class FirestoreClass {
             }
     }
 
-    fun addUpdateTaskList(activity: TaskListActivity, board: Board) {
+    fun addUpdateTaskList(activity: Activity, board: Board) {
         val taskListHashMap = HashMap<String, Any>()
         taskListHashMap[Constants.TASK_LIST] = board.taskList
 
         mFireStore.collection(Constants.BOARD).document(board.documentId).update(taskListHashMap)
             .addOnSuccessListener {
                 Log.e(activity.javaClass.simpleName, "Task updated successfully")
-                activity.addUpdateTaskListSuccess()
+                if(activity is TaskListActivity) {
+                    activity.addUpdateTaskListSuccess()
+                }
+                else if(activity is CardDetailsActivity) {
+                    activity.addUpdateTaskListSuccess()
+                }
             }.addOnFailureListener { exception ->
-                activity.hideProgressDialog()
+                if(activity is TaskListActivity) {
+                    activity.hideProgressDialog()
+                }
+                else if(activity is CardDetailsActivity) {
+                    activity.hideProgressDialog()
+                }
                 Log.e(activity.javaClass.simpleName, "Error while creating a board", exception)
             }
     }
